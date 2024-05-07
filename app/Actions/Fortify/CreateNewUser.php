ded0d5a2bd69ab9ca9b24439fde1c9c3114ffb03
Violet Lauro <violetsnow08@gmail.com>
2024-05-07 19:41:28 -0400

    Init

---
+++ app/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Laravel\Jetstream\Jetstream;
+
+class CreateNewUser implements CreatesNewUsers
+{
+    use PasswordValidationRules;
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param  array<string, string>  $input
+     */
+    public function create(array $input): User
+    {
+        Validator::make($input, [
+            'username' => ['required', 'string', 'max:20', 'unique:users'],
+//            'display_name' => [string', 'max:30'],
+            'password' => $this->passwordRules(),
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+        ])->validate();
+
+        return User::create([
+            'username' => $input['username'],
+            'display_name' => $input['username'],
+            'password' => Hash::make($input['password']),
+        ]);
+    }
+}
