a8d304ec711f846357cd9dd390c426571aa1fab4
Violet Lauro <violetsnow08@gmail.com>
2024-05-08 22:25:28 -0400

    Added VerticalMenu entry

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::get('lang/{locale}', [LanguageController::class, 'swap']);
 Route::get('/pages/misc-error', [MiscError::class, 'index'])->name('pages-misc-error');
 
 // Admin/Users
-Route::get('/admin/users', [\App\Http\Controllers\Admin\Users\UsersController::class, 'index'])->name('routes.admin.users.index');
+Route::get('/admin/users', [\App\Http\Controllers\Admin\Users\UsersController::class, 'index'])->name('routes.content.admin.users.manage');
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
