3520900daa93da12b2a87c2160fe45a7435d9086
Violet Lauro <violetsnow08@gmail.com>
2024-05-09 22:37:49 -0400

    Added icon, description, and priority to role seeder

--- database/seeders/Authentication/RoleSeeder.php
+++ database/seeders/Authentication/RoleSeeder.php
@@ -9,7 +9,7 @@ class RoleSeeder extends Seeder
   public function run(): void
   {
     //
-    $user = \Spatie\Permission\Models\Role::create(['name' => 'User']);
-    $webmaster = \Spatie\Permission\Models\Role::create(['name' => 'Webmaster']);
+    $user = \Spatie\Permission\Models\Role::create(['name' => 'User', 'description' => 'User', 'priority' => 1]);
+    $webmaster = \Spatie\Permission\Models\Role::create(['name' => 'Webmaster', 'description' => 'Webmaster', 'priority' => 100]);
   }
 }
