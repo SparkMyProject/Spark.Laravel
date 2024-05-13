dcbbc600a258933ade202866d30fab13f93b9865
Violet Lauro <violetsnow08@gmail.com>
2024-05-12 23:14:57 -0400

    Added more permissions to permissions seeder

--- database/seeders/Authentication/PermissionSeeder.php
+++ database/seeders/Authentication/PermissionSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders\Authentication;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class PermissionSeeder extends Seeder
@@ -14,7 +14,19 @@ class PermissionSeeder extends Seeder
    */
   public function run(): void
   {
-    $dashboardPermission = \Spatie\Permission\Models\Permission::create(['name' => 'actions.dashboard.index.view', 'description' => 'View the dashboard']);
+    $dashboardPermission = Permission::create(['name' => 'actions.dashboard.index.view', 'description' => 'View the dashboard']);
+    Permission::create(['name' => 'actions.admin.users.view', 'description' => 'View users']);
+    Permission::create(['name' => 'actions.admin.users.edit', 'description' => 'Edit users']);
+    Permission::create(['name' => 'actions.admin.users.disable', 'description' => 'Disable users']);
+    Permission::create(['name' => 'actions.admin.users.enable', 'description' => 'Enable users']);
+    Permission::create(['name' => 'actions.admin.settings.auditlog.view', 'description' => 'View audit log']);
+    Permission::create(['name' => 'actions.admin.settings.roles.view', 'description' => 'View roles']);
+    Permission::create(['name' => 'actions.admin.settings.roles.edit', 'description' => 'Edit roles']);
+    Permission::create(['name' => 'actions.admin.settings.roles.edit-permissions', 'description' => 'Edit role permissions']);
+    Permission::create(['name' => 'actions.admin.settings.permissions.view', 'description' => 'View permissions']);
+    Permission::create(['name' => 'actions.admin.settings.permissions.edit', 'description' => 'Edit permissions']);
+
+
     $userRole = Role::findByName('User');
     $userRole->givePermissionTo($dashboardPermission);
   }
