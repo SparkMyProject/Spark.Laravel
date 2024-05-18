e250b62cf0e703daa13a39b1af8abcffd5473d38
Violet Lauro <violetsnow08@gmail.com>
2024-05-18 18:07:03 -0400

    Upgraded to Laravel 11.x

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,29 +2,41 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Console\Kernel as ConsoleKernelContract;
+use Illuminate\Contracts\Debug\ExceptionHandler as ExceptionHandlerContract;
+use Illuminate\Contracts\Http\Kernel as HttpKernelContract;
 use Illuminate\Support\ServiceProvider;
+use App\Http\Kernel as HttpKernel;
+use App\Console\Kernel as ConsoleKernel;
+use App\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
 {
   /**
    * Register any application services.
+   *
+   * @return void
    */
-  public function register(): void
+  public function register()
   {
-    //
+    $this->app->singleton(HttpKernelContract::class, HttpKernel::class);
+    $this->app->singleton(ConsoleKernelContract::class, ConsoleKernel::class);
+    $this->app->singleton(ExceptionHandlerContract::class, ExceptionHandler::class);
   }
 
   /**
    * Bootstrap any application services.
+   *
+   * @return void
    */
-  public function boot(): void
+  public function boot()
   {
     Vite::useStyleTagAttributes(function (?string $src, string $url, ?array $chunk, ?array $manifest) {
       if ($src !== null) {
         return [
           'class' => preg_match("/(resources\/assets\/vendor\/scss\/(rtl\/)?core)-?.*/i", $src) ? 'template-customizer-core-css' :
-                    (preg_match("/(resources\/assets\/vendor\/scss\/(rtl\/)?theme)-?.*/i", $src) ? 'template-customizer-theme-css' : '')
+            (preg_match("/(resources\/assets\/vendor\/scss\/(rtl\/)?theme)-?.*/i", $src) ? 'template-customizer-theme-css' : '')
         ];
       }
       return [];
---
+++ bootstrap/providers.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    App\Providers\EventServiceProvider::class,
+    App\Providers\FortifyServiceProvider::class,
+    App\Providers\JetstreamServiceProvider::class,
+    App\Providers\MenuServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
+    SocialiteProviders\Manager\ServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
+];
