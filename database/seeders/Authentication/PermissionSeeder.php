2f23553ceffdc66c6609af054f108f86a1a55d6a
Violet Lauro <violetsnow08@gmail.com>
2024-05-20 22:20:31 -0400

    Updated permissions naming scheme

--- database/seeders/Authentication/PermissionSeeder.php
+++ database/seeders/Authentication/PermissionSeeder.php
@@ -14,28 +14,28 @@ class PermissionSeeder extends Seeder
    */
   public function run(): void
   {
-    $dashboardPermission = Permission::create(['name' => 'actions.dashboard.index.view', 'description' => 'View the dashboard', 'is_system' => true]);
+    $dashboardPermission = Permission::create(['name' => 'dashboard.view', 'description' => 'View the dashboard', 'is_system' => true]);
 
     // Admin/Users
-    Permission::create(['name' => 'actions.admin.users.view', 'description' => 'View users', 'is_system' => true]);
-    Permission::create(['name' => 'actions.admin.users.edit', 'description' => 'Edit users', 'is_system' => true]);
-    Permission::create(['name' => 'actions.admin.users.disable', 'description' => 'Disable users', 'is_system' => true]);
-    Permission::create(['name' => 'actions.admin.users.enable', 'description' => 'Enable users', 'is_system' => true]);
-    Permission::create(['name' => 'actions.admin.users.edit-roles', 'description' => 'Edit user roles', 'is_system' => true]);
+    Permission::create(['name' => 'admin.users.view', 'description' => 'View users', 'is_system' => true]);
+    Permission::create(['name' => 'admin.users.edit', 'description' => 'Edit users', 'is_system' => true]);
+    Permission::create(['name' => 'admin.users.disable', 'description' => 'Disable users', 'is_system' => true]);
+    Permission::create(['name' => 'admin.users.enable', 'description' => 'Enable users', 'is_system' => true]);
+    Permission::create(['name' => 'admin.users.edit-roles', 'description' => 'Edit user roles', 'is_system' => true]);
 
     // Admin/Settings/AuditLog
-    Permission::create(['name' => 'actions.admin.settings.auditlog.view', 'description' => 'View audit log', 'is_system' => true]);
+    Permission::create(['name' => 'admin.settings.auditlog.view', 'description' => 'View audit log', 'is_system' => true]);
 
     // Admin/Settings/Roles
-    Permission::create(['name' => 'actions.admin.settings.roles.view', 'description' => 'View roles', 'is_system' => true]);
-    Permission::create(['name' => 'actions.admin.settings.roles.edit', 'description' => 'Edit roles', 'is_system' => true]);
-    Permission::create(['name' => 'actions.admin.settings.roles.edit-permissions', 'description' => 'Edit role permissions', 'is_system' => true]);
-    Permission::create(['name' => 'actions.admin.settings.roles.delete', 'description' => 'Delete roles', 'is_system' => true]);
-    Permission::create(['name' => 'actions.admin.settings.roles.create', 'description' => 'Create roles', 'is_system' => true]);
+    Permission::create(['name' => 'admin.settings.roles.view', 'description' => 'View roles', 'is_system' => true]);
+    Permission::create(['name' => 'admin.settings.roles.edit', 'description' => 'Edit roles', 'is_system' => true]);
+    Permission::create(['name' => 'admin.settings.roles.edit-permissions', 'description' => 'Edit role permissions', 'is_system' => true]);
+    Permission::create(['name' => 'admin.settings.roles.delete', 'description' => 'Delete roles', 'is_system' => true]);
+    Permission::create(['name' => 'admin.settings.roles.create', 'description' => 'Create roles', 'is_system' => true]);
 
     // Admin/Settings/Permissions
-    Permission::create(['name' => 'actions.admin.settings.permissions.view', 'description' => 'View permissions', 'is_system' => true]);
-    Permission::create(['name' => 'actions.admin.settings.permissions.edit', 'description' => 'Edit permissions', 'is_system' => true]);
+    Permission::create(['name' => 'admin.settings.permissions.view', 'description' => 'View permissions', 'is_system' => true]);
+    Permission::create(['name' => 'admin.settings.permissions.edit', 'description' => 'Edit permissions', 'is_system' => true]);
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -47,34 +47,34 @@ Route::get('/misc/errors/not-found', function () {
 })->name('routes.misc.errors.not-found');
 
 // Admin/Users
-Route::get('/admin/users/view/{id}', [\App\Http\Controllers\Admin\Users\UsersController::class, 'view_user'])->name('routes.content.admin.users.view')->can("actions.admin.users.view");
+Route::get('/admin/users/view/{id}', [\App\Http\Controllers\Admin\Users\UsersController::class, 'view_user'])->name('routes.content.admin.users.view')->can("admin.users.view");
 
-Route::get('/admin/users', [\App\Http\Controllers\Admin\Users\UsersController::class, 'index'])->name('routes.content.admin.users.index')->can("actions.admin.users.view");
-Route::post('/admin/users/disable', [\App\Http\Controllers\Admin\Users\UsersController::class, 'disable_user'])->name('routes.content.admin.users.disable')->can("actions.admin.users.disable");
+Route::get('/admin/users', [\App\Http\Controllers\Admin\Users\UsersController::class, 'index'])->name('routes.content.admin.users.index')->can("admin.users.view");
+Route::post('/admin/users/disable', [\App\Http\Controllers\Admin\Users\UsersController::class, 'disable_user'])->name('routes.content.admin.users.disable')->can("admin.users.disable");
 Route::post('/admin/users/enable', [\App\Http\Controllers\Admin\Users\UsersController::class, 'enable_user'])->name('routes.content.admin.users.enable')->can("actions.admin.users.enable");
-Route::post('/admin/users/edit', [\App\Http\Controllers\Admin\Users\UsersController::class, 'edit_user'])->name('routes.content.admin.users.edit')->can("actions.admin.users.edit");
-Route::post('/admin/users/edit-roles/{id}', [\App\Http\Controllers\Admin\Users\UsersController::class, 'edit_roles'])->name('routes.content.admin.users.edit-roles')->can("actions.admin.settings.users.edit-roles");
+Route::post('/admin/users/edit', [\App\Http\Controllers\Admin\Users\UsersController::class, 'edit_user'])->name('routes.content.admin.users.edit')->can("admin.users.edit");
+Route::post('/admin/users/edit-roles/{id}', [\App\Http\Controllers\Admin\Users\UsersController::class, 'edit_roles'])->name('routes.content.admin.users.edit-roles')->can("admin.settings.users.edit-roles");
 
 // Admin/Settings
-Route::get('/admin/settings/auditlog', [\App\Http\Controllers\Admin\Settings\AuditLogController::class, 'index'])->name('routes.content.admin.settings.auditlog.index')->can("actions.admin.settings.auditlog.view");
-Route::get('/admin/settings/auditlog/view/{id}', [\App\Http\Controllers\Admin\Settings\AuditLogController::class, 'view'])->name('routes.content.admin.settings.auditlog.view')->can("actions.admin.settings.auditlog.view");
+Route::get('/admin/settings/auditlog', [\App\Http\Controllers\Admin\Settings\AuditLogController::class, 'index'])->name('routes.content.admin.settings.auditlog.index')->can("admin.settings.auditlog.view");
+Route::get('/admin/settings/auditlog/view/{id}', [\App\Http\Controllers\Admin\Settings\AuditLogController::class, 'view'])->name('routes.content.admin.settings.auditlog.view')->can("admin.settings.auditlog.view");
 
-Route::get('/admin/settings/roles', [\App\Http\Controllers\Admin\Settings\RolesController::class, 'index'])->name('routes.content.admin.settings.roles.index')->can("actions.admin.settings.roles.view");
-Route::post('/admin/settings/roles/edit/{id}', [\App\Http\Controllers\Admin\Settings\RolesController::class, 'edit'])->name('routes.content.admin.settings.roles.edit')->can("actions.admin.settings.roles.edit");
-Route::post('/admin/settings/roles/delete/{id}', [\App\Http\Controllers\Admin\Settings\RolesController::class, 'delete'])->name('routes.content.admin.settings.roles.delete')->can("actions.admin.settings.roles.delete");
-Route::post('/admin/settings/roles/create', [\App\Http\Controllers\Admin\Settings\RolesController::class, 'create'])->name('routes.content.admin.settings.roles.create')->can("actions.admin.settings.roles.create");
-Route::post('admin/settings/roles/edit-permissions/{id}', [\App\Http\Controllers\Admin\Settings\RolesController::class, 'edit_permissions'])->name('routes.content.admin.settings.roles.edit-permissions')->can("actions.admin.settings.roles.edit-permissions");
+Route::get('/admin/settings/roles', [\App\Http\Controllers\Admin\Settings\RolesController::class, 'index'])->name('routes.content.admin.settings.roles.index')->can("admin.settings.roles.view");
+Route::post('/admin/settings/roles/edit/{id}', [\App\Http\Controllers\Admin\Settings\RolesController::class, 'edit'])->name('routes.content.admin.settings.roles.edit')->can("admin.settings.roles.edit");
+Route::post('/admin/settings/roles/delete/{id}', [\App\Http\Controllers\Admin\Settings\RolesController::class, 'delete'])->name('routes.content.admin.settings.roles.delete')->can("admin.settings.roles.delete");
+Route::post('/admin/settings/roles/create', [\App\Http\Controllers\Admin\Settings\RolesController::class, 'create'])->name('routes.content.admin.settings.roles.create')->can("admin.settings.roles.create");
+Route::post('admin/settings/roles/edit-permissions/{id}', [\App\Http\Controllers\Admin\Settings\RolesController::class, 'edit_permissions'])->name('routes.content.admin.settings.roles.edit-permissions')->can("admin.settings.roles.edit-permissions");
 
 
 Route::get('/admin/settings/permissions', [\App\Http\Controllers\Admin\Settings\PermissionsController::class, 'index'])->name('routes.content.admin.settings.permissions.index')->can("actions.admin.settings.permissions.view");
 Route::post('/admin/settings/permissions/edit/{id}', [\App\Http\Controllers\Admin\Settings\PermissionsController::class, 'edit'])->name('routes.content.admin.settings.permissions.edit')->can("actions.admin.settings.permissions.edit");
-Route::get('/admin/settings/permissions/view/{id}', [\App\Http\Controllers\Admin\Settings\PermissionsController::class, 'view'])->name('routes.content.admin.settings.permissions.view')->can("actions.admin.settings.permissions.view");
+Route::get('/admin/settings/permissions/view/{id}', [\App\Http\Controllers\Admin\Settings\PermissionsController::class, 'view'])->name('routes.content.admin.settings.permissions.view')->can("admin.settings.permissions.view");
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-  Route::get('/dashboard', [\App\Http\Controllers\Dashboard\HomePage::class, 'index'])->name('routes.content.dashboard.index')->can("actions.dashboard.index.view");
+  Route::get('/dashboard', [\App\Http\Controllers\Dashboard\HomePage::class, 'index'])->name('routes.content.dashboard.index')->can("dashboard.view");
 });
 
 Route::get('/auth/discord/redirect', function () {
