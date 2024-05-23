167433db2bdd97e0a26da1aa85a968772c94f12a
Violet Lauro <violetsnow08@gmail.com>
2024-05-23 10:32:29 -0400

    Added DebugBar

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,13 +1,14 @@
 <?php
 
 return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\AuthServiceProvider::class,
-    App\Providers\EventServiceProvider::class,
-    App\Providers\FortifyServiceProvider::class,
-    App\Providers\JetstreamServiceProvider::class,
-    App\Providers\MenuServiceProvider::class,
-    App\Providers\RouteServiceProvider::class,
-    SocialiteProviders\Manager\ServiceProvider::class,
-    Spatie\Permission\PermissionServiceProvider::class,
+  App\Providers\AppServiceProvider::class,
+  App\Providers\AuthServiceProvider::class,
+  App\Providers\EventServiceProvider::class,
+  App\Providers\FortifyServiceProvider::class,
+  App\Providers\JetstreamServiceProvider::class,
+  App\Providers\MenuServiceProvider::class,
+  App\Providers\RouteServiceProvider::class,
+  SocialiteProviders\Manager\ServiceProvider::class,
+  Spatie\Permission\PermissionServiceProvider::class,
+  Barryvdh\Debugbar\ServiceProvider::class,
 ];
--- resources/views/vendor/pulse/dashboard.blade.php
+++ resources/views/vendor/pulse/dashboard.blade.php
@@ -16,4 +16,6 @@
     <livewire:pulse.slow-jobs cols="6" />
 
     <livewire:pulse.slow-outgoing-requests cols="6" />
+
+    <livewire:pulse.validation-errors cols="8" rows="4" />
 </x-pulse>
