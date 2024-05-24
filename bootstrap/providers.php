4945e20cc0d8223175595c136154e4a1ef41571b
Violet Lauro <violetsnow08@gmail.com>
2024-05-24 00:39:02 -0400

    Added Laravel Horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,14 +1,15 @@
 <?php
 
 return [
-  App\Providers\AppServiceProvider::class,
-  App\Providers\AuthServiceProvider::class,
-  App\Providers\EventServiceProvider::class,
-  App\Providers\FortifyServiceProvider::class,
-  App\Providers\JetstreamServiceProvider::class,
-  App\Providers\MenuServiceProvider::class,
-  App\Providers\RouteServiceProvider::class,
-  SocialiteProviders\Manager\ServiceProvider::class,
-  Spatie\Permission\PermissionServiceProvider::class,
-  Barryvdh\Debugbar\ServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    App\Providers\EventServiceProvider::class,
+    App\Providers\FortifyServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\JetstreamServiceProvider::class,
+    App\Providers\MenuServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
+    Barryvdh\Debugbar\ServiceProvider::class,
+    SocialiteProviders\Manager\ServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
 ];
