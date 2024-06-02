9c0c392ca3fa60d88ae0ccf424296def9e1ffb4d
Violet Lauro <violetsnow08@gmail.com>
2024-06-02 19:54:12 -0400

    Added VerifyCsrfToken middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -42,7 +42,7 @@ return Application::configure(basePath: $_ENV['APP_BASE_PATH'] ?? dirname(__DIR_
       \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
       \Illuminate\Session\Middleware\StartSession::class,
       \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-//      \App\Http\Middleware\VerifyCsrfToken::class,
+      \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
       \Illuminate\Routing\Middleware\SubstituteBindings::class,
       \App\Http\Middleware\LocaleMiddleware::class,
     ]);
