ad78bb168664102b53576ddb433db7526d27d739
Violet Lauro <violetsnow08@gmail.com>
2024-06-02 17:41:46 -0400

    Updated RouteServiceProvider and bootstrap/app.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -27,32 +27,5 @@ class RouteServiceProvider extends ServiceProvider
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
-
-        $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web.php'));
-
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web/admin/settings.php'));
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web/admin/users.php'));
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web/authentication.php'));
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/jetstream.php'));
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web/dashboard/dashboard.php'));
-        });
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__))
   ->withRouting(
@@ -11,6 +12,18 @@ return Application::configure(basePath: $_ENV['APP_BASE_PATH'] ?? dirname(__DIR_
     commands: __DIR__.'/../routes/console.php',
         channels: __DIR__.'/../routes/channels.php',
     health: '/up',
+    then: function () {
+      Route::middleware('web')
+        ->group(base_path('routes/web/admin/settings.php'));
+      Route::middleware('web')
+        ->group(base_path('routes/web/admin/users.php'));
+      Route::middleware('web')
+        ->group(base_path('routes/web/authentication.php'));
+      Route::middleware('web')
+        ->group(base_path('routes/jetstream.php'));
+      Route::middleware('web')
+        ->group(base_path('routes/web/dashboard/dashboard.php'));
+    }
   )
   ->withMiddleware(function (Middleware $middleware) {
     // Add middleware configurations here if needed
