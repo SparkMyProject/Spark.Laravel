f0bdc8d93622cc2947bf893822d2ae4c7975aae4
Violet Lauro <violetsnow08@gmail.com>
2024-06-02 19:52:26 -0400

    Added Calendar controller

--- app/Http/Controllers/Dashboard/Calendar.php
+++ app/Http/Controllers/Dashboard/Calendar.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use Barryvdh\Debugbar\Facades\Debugbar;
 
 class Calendar extends Controller
 {
   public function calendar()
   {
-    return view('content.dashboard.calendar');
+    $events = \App\Models\CalendarEvent::all();
+    Debugbar::info($events);
+    return view('content.dashboard.calendar', compact('events'));
   }
 }
