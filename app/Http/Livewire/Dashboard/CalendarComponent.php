5c7bbde9d4d07d0ef5bfe4b31b98ea2e4eb75108
Violet Lauro <violetsnow08@gmail.com>
2024-06-04 17:43:13 -0400

    Push for mobile dev

--- app/Http/Livewire/Dashboard/CalendarComponent.php
+++ app/Http/Livewire/Dashboard/CalendarComponent.php
@@ -3,24 +3,42 @@
 namespace App\Http\Livewire\Dashboard;
 
 use Carbon\Carbon;
-use DateTime;
 use Livewire\Component;
 
 class CalendarComponent extends Component
 {
+  public $events;
+  public $displayedEvents;
 
+//  public function mount($events)
+//  {
+//    $this->events = $events;
+//  }
 
-  public function render()
+  public function mount()
   {
-    $events = \App\Models\CalendarEvent::query()
+
+    $this->events = \App\Models\CalendarEvent::query()
       ->select('id', 'calendar_id', 'user_id', 'title', 'label', 'start_date AS start', 'end_date AS end', 'all_day AS allDay', 'url', 'description')
       ->get();
 
-    $events->transform(function ($event) {
+    $this->events->transform(function ($event) {
       $event->start = Carbon::parse($event->start)->inUserTimezone();
       $event->end = Carbon::parse($event->end)->inUserTimezone();
       return $event;
     });
-    return view('components.livewire.dashboard.calendar-component', ['events' => $events]);
+
+    $this->displayedEvents = $this->events;
+
+  }
+
+  public function render()
+  {
+    return view('components.livewire.dashboard.calendar-component', ['events' => $this->displayedEvents]);
+  }
+
+  public function testEvent()
+  {
+    dd('create_event');
   }
 }
