53c5f4a4ca24c50684470e0d7978247c0f420c23
Violet Lauro <violetsnow08@gmail.com>
2024-06-15 22:01:18 -0400

    Fixed something

--- app/Http/Controllers/Web/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Web/Auth/VerifyEmailController.php
@@ -11,83 +11,96 @@ use Prologue\Alerts\Facades\Alert;
 
 class VerifyEmailController extends Controller
 {
-    public null|string $redirectTo = null;
+  public null|string $redirectTo = null;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        if (! app('router')->getMiddleware()['signed'] ?? null) {
-            throw new Exception('Missing "signed" alias middleware in App/Http/Kernel.php. More info: https://backpackforlaravel.com/docs/6.x/base-how-to#enable-email-verification-in-backpack-routes');
-        }
-
-        $this->middleware('signed')->only('verifyEmail');
-        $this->middleware('throttle:'.config('backpack.base.email_verification_throttle_access'))->only('resendVerificationEmail');
-
-        if (! backpack_users_have_email()) {
-            abort(500, trans('backpack::base.no_email_column'));
-        }
-        // where to redirect after the email is verified
-        $this->redirectTo = $this->redirectTo ?? route('backpack.auth.login');
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    if (!app('router')->getMiddleware()['signed'] ?? null) {
+      throw new Exception('Missing "signed" alias middleware in App/Http/Kernel.php. More info: https://backpackforlaravel.com/docs/6.x/base-how-to#enable-email-verification-in-backpack-routes');
     }
 
-    public function emailVerificationRequired(Request $request): \Illuminate\Contracts\View\View|\Illuminate\Http\RedirectResponse
-    {
-        $this->getUserOrRedirect($request);
+    $this->middleware('signed')->only('verifyEmail');
+    $this->middleware('throttle:' . config('backpack.base.email_verification_throttle_access'))->only('resendVerificationEmail');
 
-        return view(backpack_view('auth.verify-email'));
+    if (!backpack_users_have_email()) {
+      abort(500, trans('backpack::base.no_email_column'));
+    }
+    // where to redirect after the email is verified
+    $this->redirectTo = $this->redirectTo ?? route('backpack.auth.login');
+  }
+
+  public function emailVerificationRequired(Request $request): \Illuminate\Contracts\View\View|\Illuminate\Http\RedirectResponse
+  {
+    $user = $this->getUserOrRedirect($request);
+
+    if (is_a($user, \Illuminate\Http\RedirectResponse::class)) { // Redirect if not a user
+      return $user;
     }
 
-    /**
-     * Verify the user's email address.
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
-    public function verifyEmail(EmailVerificationRequest $request)
-    {
-        $this->getUserOrRedirect($request);
+    return view(backpack_view('auth.verify-email'));
+  }
 
-        $request->fulfill();
+  /**
+   * Verify the user's email address.
+   *
+   * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+   */
+  public function verifyEmail(EmailVerificationRequest $request)
+  {
+    dd("hello");
+    $user = $this->getUserOrRedirect($request);
+    if (is_a($user, \Illuminate\Http\RedirectResponse::class)) { // Redirect if not a user
+      return $user;
+    }
+    $request->fulfill();
 
-        return redirect($this->redirectTo);
+    return redirect($this->redirectTo);
+  }
+
+  /**
+   * Resend the email verification notification.
+   */
+  public function resendVerificationEmail(Request $request): \Illuminate\Http\RedirectResponse
+  {
+    $user = $this->getUserOrRedirect($request);
+
+    if (is_a($user, \Illuminate\Http\RedirectResponse::class)) { // Redirect if not a user
+      return $user;
     }
 
-    /**
-     * Resend the email verification notification.
-     */
-    public function resendVerificationEmail(Request $request): \Illuminate\Http\RedirectResponse
-    {
-        $user = $this->getUserOrRedirect($request);
+//    if ($user->hasVerifiedEmail()) {
+//      Alert::info('Your email address is already verified.')->flash();
+//      return redirect($this->redirectTo);
+//    }
 
-        if (is_a($user, \Illuminate\Http\RedirectResponse::class)) { // Redirect if not a user
-            return $user;
-        }
+    $user->sendEmailVerificationNotification();
+    Alert::success('Email verification link sent successfully.')->flash();
 
-        if ($user->hasVerifiedEmail()) {
-            Alert::info('Your email address is already verified.')->flash();
-            return redirect($this->redirectTo);
-        }
+    return back()->with('status', 'verification-link-sent');
+  }
 
-        $user->sendEmailVerificationNotification();
-        Alert::success('Email verification link sent successfully.')->flash();
+  private function getUser(Request $request): ?\Illuminate\Contracts\Auth\MustVerifyEmail
+  {
+    return $request->user(backpack_guard_name()) ?? (new UserFromCookie())();
+  }
 
-        return back()->with('status', 'verification-link-sent');
+  private function getUserOrRedirect(Request $request): \Illuminate\Contracts\Auth\MustVerifyEmail|\Illuminate\Http\RedirectResponse
+  {
+    $user = $this->getUser($request);
+    if (!$user) {
+      Alert::error('You are currently not logged in.')->flash();
     }
-
-    private function getUser(Request $request): ?\Illuminate\Contracts\Auth\MustVerifyEmail
-    {
-        return $request->user(backpack_guard_name()) ?? (new UserFromCookie())();
+    if ($user && $user->hasVerifiedEmail()) {
+      Alert::error('Your email address is already verified.')->flash();
     }
-
-    private function getUserOrRedirect(Request $request): \Illuminate\Contracts\Auth\MustVerifyEmail|\Illuminate\Http\RedirectResponse
-    {
-        if ($user = $this->getUser($request)) {
-            return $user;
-        }
-
-        return redirect()->route('backpack.auth.login');
+    if ($user && !$user->hasVerifiedEmail()) {
+      return $user;
     }
+    return redirect()->route('backpack.auth.login');
+  }
 }
